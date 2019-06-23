d24c94de5fbb69d5707c9c35b3b576fe269bd0f6
Marcelo Wippel <mawippel2@hotmail.com>
2019-06-23 17:38:56 -0300

    lambdas

--- src/main/java/designpatterns/interfaces/lambdas/CheckAnimal.java
+++ src/main/java/designpatterns/interfaces/lambdas/CheckAnimal.java
@@ -1,11 +1,12 @@
 package designpatterns.interfaces.lambdas;
 
 /**
- * A functional interface containing a method that receives an Animal and can execute any method inside it 
+ * A functional interface containing a method that receives an Animal and can
+ * execute any method inside it
  */
 @FunctionalInterface
 public interface CheckAnimal {
 
 	boolean test(Animal a);
-	
+
 }
--- src/main/java/designpatterns/interfaces/lambdas/LambdasMain.java
+++ src/main/java/designpatterns/interfaces/lambdas/LambdasMain.java
@@ -1,15 +1,23 @@
 package designpatterns.interfaces.lambdas;
 
+import java.util.function.Predicate;
+
 public class LambdasMain {
 
-	private static void print(Animal a, CheckAnimal check) {
+	private static void print(Animal a, Predicate<Animal> check) {
 		if (check.test(a)) {
 			System.out.println(a);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		print(new Animal("a", true, true), b -> b.canSwim());
-		print(new Animal("a", true, true), b -> b.canHop());
+		print(new Animal("a", true, true), b -> {
+			return b.canSwim();
+		});
+		print(new Animal("a", true, true), (Animal b) -> {
+			return b.canSwim();
+		});
+		print(new Animal("a", true, true), b -> true);
 	}
 }
