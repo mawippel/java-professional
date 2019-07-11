22418fa7d97a38332752871cef39c66d18d5f86b
Marcelo Wippel <mawippel2@hotmail.com>
2019-07-11 17:06:57 -0300

    exceptions

---
+++ src/main/java/exceptions/Exceptions.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+import java.sql.SQLException;
+import java.time.DateTimeException;
+
+import javax.management.RuntimeErrorException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ * Checked Exceptions: require try-catch. Extends Exception
+ * Unchecked Exceptions: doesnt require try-catch. Extends RuntimeException
+ */
+public class Exceptions {
+
+	private void multiCatch(Integer x) {
+		try {
+			if (x == 2) {
+				throw new UnsupportedAudioFileException();
+			} else {
+				throw new DateTimeException("aaaahhhh");
+			}
+		} catch (UnsupportedAudioFileException | DateTimeException e) { // valid multicatch
+//			e = new RuntimeException(); it's not valid, as the exception in a multicatch is effectvily final
+		}
+	}
+	
+}
